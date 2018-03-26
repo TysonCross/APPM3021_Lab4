457d09c33813cf782d2d4be553c0d737a2ca8a3f
Tyson <tyson.cross@gmail.com>
2018-03-26 21:39:36 +0200

    Exercise 1 complete

--- code/NewtonInterp.m
+++ code/NewtonInterp.m
@@ -4,14 +4,19 @@
 % containing the polynomial coefficient values in 'co' and the table of
 % differences in matrix T
 
-% x entries are the x values
-% y entries are value at f(x)
+    % x entries are the x values
+    % y entries are value at f(x)
 
-[n. m] = size(x); % m is the number of data points
-
-for i=1:m
-    for j=1:i
+    [n m] = size(x); % m is the number of data points
+    T = zeros(n, n);
+    T(:,1) = double(y)';
+    for j=2:n
+        for i=1:(n-j+1)
+            T(i,j) = ( T(i+1,j-1) - T(i,j-1) ) / ( x(i+j-1) - x(i) );
+        end
     end
+    co = T(1,:);
 end
-end
+
+
 
---
+++ code/exercise1.m
@@ -0,0 +1,35 @@
+% APPM3021 Lab 4 Exercise 1
+% Tyson Cross 1239448
+
+clc
+clear all
+format short g
+format compact
+rng('shuffle');
+
+N = 5;
+syms f a;
+f(a) = 1/3*a.^4 + 2*a.^3 - 5*sin(2*a) + exp(-a/2) - 12;
+
+x = sort(unifrnd(0,5,N,1))
+y = double(f(x))
+
+[co, T] = NewtonInterp(x,y)
+
+% output
+
+fprintf('\nP(x) = ')
+for i=1:length(co)
+    fprintf('%2f',co(i));
+    if i ~=1  
+        for j=1:i-1
+            fprintf('(x-%d)',x(j));
+        end
+    end
+    
+    if i~=length(co)
+        fprintf(' + ')
+    else
+        fprintf('\n')
+    end
+end
